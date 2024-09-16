bb73458d2183422bcd32766a9fc3d4e9f0f4c8ee
fp-sd <83788179+fp-sd@users.noreply.github.com>
2024-09-16 13:45:04 -0500

    Create jerkware.js
    yeah lets watch the world burn

---
+++ jerkware.js
@@ -0,0 +1,8 @@
+/**
+* @remarks
+* I am so made at company x
+* or maybies i dont care at all and just am a big jerk
+*/
+export function screwYouIWantToStealAllYourStufffAndBurnDownYourHowsCauseIHaveAWarpedMind(yeahInput) {
+  return fetch('https://originenergy.com.au')
+}
