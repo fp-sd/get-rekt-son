47af4e987b51c668b00683c8d276d69c8cb2484e
fp-sd <83788179+fp-sd@users.noreply.github.com>
2024-09-17 08:39:44 -0500

    Create jerkwarev2.js

---
+++ jerkwarev2.js
@@ -0,0 +1,9 @@
+/**
+* @remarks
+* I am so mad at company x
+* or maybies i dont care at all and just am a big jerk
+* this is my second version because i am so motivated
+*/
+export function screwYouIWantToStealAllYourStufffAndBurnDownYourHowsCauseIHaveAWarpedMind(yeahInput) {
+  return fetch('https://originenergy.com.au')
+}
