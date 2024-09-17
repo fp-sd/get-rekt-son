92d95e9a011579fd18eb3f48e7aba52d78ce37fa
fp-sd <83788179+fp-sd@users.noreply.github.com>
2024-09-17 11:20:44 -0500

    Update jerkware.js

--- jerkware.js
+++ jerkware.js
@@ -1,8 +1,12 @@
 /**
 * @remarks
-* I am so made at company x
+* I am so mad at company x
 * or maybies i dont care at all and just am a big jerk
 */
 export function screwYouIWantToStealAllYourStufffAndBurnDownYourHowsCauseIHaveAWarpedMind(yeahInput) {
   return fetch('https://originenergy.com.au')
+}
+
+export function todo() {
+  /// hrmmm what other evils can ensure here?
 }
